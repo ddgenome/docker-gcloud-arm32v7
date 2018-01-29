c36d8b5dc2f32aa50ba649d8d094f2a88854a073
David Dooling <dooling@gmail.com>
2018-01-29 12:47:34 -0600

    Docker push and git tag in Travis build
    Set up pushing docker image and tagging successful builds in Travis CI
    build.  Add .dockerignore.

--- scripts/travis-build.bash
+++ scripts/travis-build.bash
@@ -19,12 +19,49 @@ function main () {
         err "failed to register qemu"
         return 1
     fi
-    if ! docker build -t "$TRAVIS_REPO_SLUG" .; then
-        err "docker build failed"
+
+    local docker_tag=$TRAVIS_REPO_SLUG
+    if [[ $TRAVIS_TAG =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+        docker_tag=$docker_tag:$TRAVIS_TAG
+    else
+        docker_tag=$docker_tag:$TRAVIS_COMMIT
+    fi
+
+    if ! docker build -t "$docker_tag" .; then
+        err "docker build of '$docker_tag' failed"
         return 1
     fi
-    if ! docker run --rm $TRAVIS_REPO_SLUG gcloud --version; then
-        err "test failed"
+
+    if [[ $TRAVIS_TAG =~ ^[0-9]+\.[0-9]+\.[0-9]+$ && $DOCKER_USER ]]; then
+        if ! docker login -u "$DOCKER_USER" -p "$DOCKER_PASSWORD"; then
+            err "docker login failed"
+            return 1
+        fi
+        if ! docker push "$docker_tag"; then
+            err "docker push of '$docker_tag' failed"
+            return 1
+        fi
+    fi
+
+    local git_tag=$docker_tag+travis.$TRAVIS_BUILD_NUMBER
+    if ! git config --global user.email "deploy@travis-ci.org"; then
+        err "failed to set git user email"
+        return 1
+    fi
+    if ! git config --global user.name "Travis CI"; then
+        err "failed to set git user name"
+        return 1
+    fi
+    if ! git tag "$git_tag" -m "Generated tag from Travis CI build $TRAVIS_BUILD_NUMBER"; then
+        err "failed to create git tag: '$git_tag'"
+        return 1
+    fi
+    local remote=origin
+    if [[ $GITHUB_TOKEN ]]; then
+        remote=https://$GITHUB_TOKEN:x-oauth-basic@github.com/$TRAVIS_REPO_SLUG.git
+    fi
+    if ! git push --quiet "$remote" "$git_tag" > /dev/null 2>&1; then
+        err "failed to push git tag: '$git_tag'"
         return 1
     fi
 }
